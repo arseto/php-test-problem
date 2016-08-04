585b30790226f47e476c27d756585d462647e8a6
Arseto Nugroho <satriyo.796@gmail.com>
2016-08-04 09:30:17 +0700

    Inital commit
    * Added Testable class Rectangle

---
+++ unit/Rectangle.php
@@ -0,0 +1,44 @@
+<?php
+namespace Unit;
+
+class Rectangle
+{
+    private $length;
+    private $width;
+
+    public function __construct($length, $width)
+    {
+        $this->length = $length;
+        $this->width = $width;
+    }
+
+    public function getLength()
+    {
+        return $this->length;
+    }
+
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    public function getArea()
+    {
+        return $this->length * $this->width;
+    }
+
+    public function getPerimeter()
+    {
+        return 2 * ($this->length + $this->width);
+    }
+
+    public function setWidth($width)
+    {
+        $this->width = $width;
+    }
+
+    public function setLength($length)
+    {
+        $this->length = $length;
+    }
+}
