474e84143e0d825c0001ac84ae5a1557584a2305
Arseto Nugroho <satriyo.796@gmail.com>
2016-08-04 09:33:49 +0700

    Added comments and change method name to distinct from getter

--- unit/Rectangle.php
+++ unit/Rectangle.php
@@ -1,6 +1,17 @@
 <?php
 namespace Unit;
 
+/**
+ * Create unit test for this class that covers:
+ *
+ * 1. Constructor test
+ * 2. Getter tests
+ * 3. Class function tests
+ * 4. Setter test
+ *
+ * Pseudocode allowed, PHP code which runs under PHPUnit is preferred,
+ * Happy Coding !!
+ */
 class Rectangle
 {
     private $length;
@@ -22,12 +33,12 @@ class Rectangle
         return $this->width;
     }
 
-    public function getArea()
+    public function calculateArea()
     {
         return $this->length * $this->width;
     }
 
-    public function getPerimeter()
+    public function calculatePerimeter()
     {
         return 2 * ($this->length + $this->width);
     }
